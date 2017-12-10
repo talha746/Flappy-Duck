639742f783f8f9989cd39207a6ba68e1cfdf6d62
Talha <talha7171@gmail.com>
2017-12-10 16:35:58 -0500

    Version 1.3 of Flappy Duck

---
+++ Version 1.3/Assets/scripts/SceneController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+	}
+}
